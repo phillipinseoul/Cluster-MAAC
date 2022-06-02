05042c590bf9a93cfbaf3e3ba04c74d90ae6208b
root <ooasd990@gmail.com>
2022-06-02 09:10:37 +0000

    clustering code added

--- utils/clustering.py
+++ utils/clustering.py
@@ -1,6 +1,9 @@
 '''TODO: clustering the agents based on the position'''
+from sklearn.cluster import KMeans
+from collections import defaultdict
 
-def cluster_agents(agents, positions):
+
+def cluster_agents(agents, positions, n_label):
     '''
     - input: 
         agents: list of agents indices
@@ -10,7 +13,13 @@ def cluster_agents(agents, positions):
         (e.g., cluster_list = {0: [1, 2, 3], 1: [4, 5, 6], 2: [7, 8]})
     '''
 
-    return
+    kmeans = KMeans(n_clusters=n_label)
+    kmeans.fit(positions)
 
+    cluster_list = defaultdict(list)
+    for label , index in zip(kmeans.labels_, agents):
+        cluster_list[label].append(index)
 
+    cluster_list = dict(map(lambda kv: (kv[0], kv[1].sort()), cluster_list.iteritems()))
 
+    return cluster_list
